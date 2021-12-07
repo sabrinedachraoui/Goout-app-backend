7037dfeedff29b98a87bc39cf1c595da0b472ad4
salahdabbech1 <58320947+salahdabbech1@users.noreply.github.com>
2021-12-07 12:29:29 +0100

    idk what happend

---
+++ models/Parent.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+//define a new data object
+
+const UserSchema = new mongoose.Schema({
+        name: { type: String },
+        email: { type: String },
+        address: { type: String },
+        password: { type: String },
+        role: { type: String },
+        Kids:[{
+                type: mongoose.SchemaTypes.ObjectId, ref: 'Kids',
+        }],
+        isVerified:{type: Boolean},
+});
+
+//we want to export this file to the server so it can see the schema
+//the data is the student we made
+module.exports = mongoose.model("parent", UserSchema);
